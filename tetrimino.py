87aa3c3c25dbef50a36f50f77e53447ea4b1a3ea
syddonym <ssyydneyyson@gmail.com>
2025-11-13 14:10:46 -1000

    fixed L block rotation

--- tetrimino.py
+++ tetrimino.py
@@ -7,7 +7,7 @@ class LBlock(Block):
         self.cells = {
             0: [Position(0, 2), Position(1, 0), Position(1,1), Position(1,2)],
             1: [Position(0, 1), Position(1, 1), Position(2, 1), Position(2, 2)],
-            2: [Position(1, 0), Position(0, 2), Position(1, 1), Position(1, 2)],
+            2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 0)],
             3: [Position(0, 0), Position(0, 1), Position(1, 1), Position(2, 1)]
         }
         self.move(0, 3)
