f1227418afbe8397848097d5f2818242de4f14b4
syddonym <ssyydneyyson@gmail.com>
2025-11-18 13:56:07 -1000

    fixed block colors

--- base_tetris/colors.py
+++ base_tetris/colors.py
@@ -1,16 +1,16 @@
 class Colors:
-    dark_grey = (26, 31, 40)
-    green = (47, 230, 23) 
-    red = (232, 18, 18) 
-    orange = (226, 116, 17) 
-    yellow = (237, 234, 4) 
-    purple = (166, 0, 247) 
-    cyan = (21, 204, 209) 
-    blue = (13, 64, 216) 
+    dark_grey = (26, 31, 40) # 0
+    green = (47, 230, 23)  # 1
+    red = (232, 18, 18)  # 2 
+    orange = (226, 116, 17)  # 3 
+    yellow = (237, 234, 4)  # 4 
+    purple = (166, 0, 247)  # 5
+    cyan = (21, 204, 209) # 6
+    blue = (13, 64, 216) # 7
 
-    dark_blue = (44, 44, 127)
-    light_blue = (59, 85, 162)
-    white = (255, 255, 255)
+    dark_blue = (44, 44, 127) # 8 
+    light_blue = (59, 85, 162) # 9 
+    white = (255, 255, 255) # 10
 
     @classmethod
     def get_cell_colors(cls):
