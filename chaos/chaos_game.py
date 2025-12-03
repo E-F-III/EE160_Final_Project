f2f9ea394b061277676763f689130bbc7ab66204
Karl Angelo D Balajadia <kabangelo2006@gmail.com>
2025-12-02 14:35:27 -1000

    Update chaos_game.py

--- chaos/chaos_game.py
+++ chaos/chaos_game.py
@@ -1,5 +1,12 @@
 from base_tetris.game import Game
+import random
 
 class Chaos(Game):
     def __init__(self):
         super().__init__()
+    
+    def lock_block(self):
+        super().lock_block()
+
+        #randomize fall speed for the next block (normal to very fast)
+        self.fall_speed = random.choice([500, 50])
